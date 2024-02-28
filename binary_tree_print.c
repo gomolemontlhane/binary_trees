310e2c41078abca4d5c9da5fe9fbb0ce8495bc9a
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-28 15:07:46 -0800

    solution for task 0. Fixed format

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -11,12 +11,17 @@ void binary_tree_print(const binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	printf("%d\n", tree->n);
+	printf("       ");
+	printf(".-------(%03d)-------.\n", tree->n);
 	if (tree->left != NULL || tree->right != NULL)
 	{
-		printf("Left:\n");
-		binary_tree_print(tree->left);
-		printf("Right:\n");
-		binary_tree_print(tree->right);
+		printf("  .--(%03d)--.         .--(%03d)--.\n",
+			(tree->left ? tree->left->n : 0),
+			(tree->right ? tree->right->n : 0));
+		printf("(%03d)     (%03d)     (%03d)     (%03d)\n",
+			(tree->left && tree->left->left ? tree->left->left->n : 0),
+			(tree->left && tree->left->right ? tree->left->right->n : 0),
+			(tree->right && tree->right->left ? tree->right->left->n : 0),
+			(tree->right && tree->right->right ? tree->right->right->n : 0));
 	}
 }
