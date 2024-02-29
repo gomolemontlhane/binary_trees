e2744df3e6f0082863b7eec6a7fcf8599d65a1c0
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-29 08:14:14 -0800

    updated header files

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <stddef.h>
 
 /*Basic Binary Tree Structure */
 typedef struct binary_tree_s
@@ -12,7 +13,9 @@ typedef struct binary_tree_s
 
 /*Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree); // Declaration for binary_tree_print
+
+/*Function to print binary tree (for visualization purposes) */
+void binary_tree_print(const binary_tree_t *);
+
 
 #endif /*BINARY_TREES_H */
