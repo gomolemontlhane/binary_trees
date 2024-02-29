495c27448936b3abd8c9230d0391c3b7fa2dd239
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-29 08:56:35 -0800

    solution for task 1

--- binary_trees.h
+++ binary_trees.h
@@ -13,6 +13,7 @@ typedef struct binary_tree_s
 
 /*Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 /*Function to print binary tree (for visualization purposes) */
 void binary_tree_print(const binary_tree_t *);
