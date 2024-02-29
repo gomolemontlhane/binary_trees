0c41d9e1beefb3169457c7852d111df037bdd69e
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-29 06:51:23 -0800

    fixing code

--- binary_trees.h
+++ binary_trees.h
@@ -8,9 +8,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}
-
-binary_tree_t;
+} binary_tree_t;
 
 /*Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
