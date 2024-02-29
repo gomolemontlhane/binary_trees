07970ef50b0bc31d5429985802f4b41f70867128
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-29 15:02:48 -0800

    Implement binary_tree_height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t left_height= 0, right_height = 0;
+		size_t left_height = 0, right_height = 0;
 
 		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
